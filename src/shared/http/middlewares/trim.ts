f29df22b208491e517a8b92ed859429ca9f00c30
Lucas Reinaldo de Melo <lucasreinaldo.demelo@hotmail.com>
2021-01-14 12:22:05 +0000

    feat: create middleware validation for blank spaces

---
+++ src/shared/http/middlewares/trim.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+
+export default (request: Request, _: Response, next: NextFunction): void => {
+  const exceptions = ['password'];
+
+  Object.keys(request.body).forEach((key) => {
+    if (!exceptions.includes(key) && typeof request.body[key] === 'string') {
+      request.body[key] = request.body[key].trim();
+    }
+  });
+
+  next();
+};
